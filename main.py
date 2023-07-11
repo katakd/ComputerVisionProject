3ae46e008d6379f566ead6728c1c76dd41bcf2ef
katakd <89955319+katakd@users.noreply.github.com>
2023-07-11 23:49:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ methods = {
 }
 assert args.method in methods, f"Method {args.method} not implemented in {methods}"
 
-if args.method == "pseudo label":
-    print("Please have a look at `pseudo/pseudo_label.ipynb` notebook")
-else:
-    print(f"Training with default configuration of method: {args.method}")
-    subprocess.run(methods[args.method], shell=True)
+
+print(f"Training with default configuration of method: {args.method}")
+subprocess.run(methods[args.method], shell=True)
